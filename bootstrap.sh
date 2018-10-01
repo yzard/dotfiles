6d89f4955976c0ba5c712a8869c97e93a2f201f1
Zhuo <zhuoyin@gmail.com>
2018-10-01 03:37:28 +0000

    changes

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,4 +5,3 @@ ln -s ~/dotfiles/bashrc ~/.bashrc
 ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 
-[ -z "~/.ssh" ] && cp -pr ~/dotfiles/ssh ~/.ssh 
--- linking.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cd ~/
-ln -s ~/dotfiles/bashrc ~/.bashrc
-ln -s ~/dotfiles/vimrc ~/.vimrc
-ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
