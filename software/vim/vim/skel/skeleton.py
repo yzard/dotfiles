16cd46f6528a94e43848dd1b042e964a0a8eea8b
Zhuo Yin <zhuoyin@gmail.com>
2019-04-21 15:03:49 -0400

    change skeleton.py

--- software/vim/vim/skel/skeleton.py
+++ software/vim/vim/skel/skeleton.py
@@ -3,6 +3,7 @@
 # vim: tabstop=4 shiftwidth=4 softtabstop=4 colorcolumn=120 expandtab
 
 import logging
+import argparse
 
 
 class %CLASS_NAME%(object):
@@ -10,16 +11,23 @@ class %CLASS_NAME%(object):
         pass
 
 
-def main(argc, argv):
-    '''
-    for test purpose if this is a module
-    '''
+def get_args():
+    parser = argparse.ArgumentParser
+    parser = argparse.ArgumentParser(description='%CLASS_NAME',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-f", "--file", help="specify file name") 
+    parser.add_argument("leftover", nargs="*")
+    return parser
+
+
+def main():
     formatter = '%(asctime)s %(filename)s:%(lineno)d: %(message)s'
     logging.basicConfig(format=formatter, level=logging.DEBUG)
+
+    parser = get_args()
 
     obj = %CLASS_NAME%()
 
 
 if __name__ == '__main__':
-    import sys
-    main(len(sys.argv), sys.argv)
+    main()
