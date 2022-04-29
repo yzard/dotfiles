f52dd0abe9872a69cced96ba31fedd194371ae60
Zhuo Yin <zhuoyin@gmail.com>
2022-04-29 00:32:13 -0400

    Add Manjaro

---
+++ software/vim/ManjaroLinux.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo pacman -S vim
+
+[ -d ~/.vim ] && mv ~/.vim ~/bak/ 
+cp -pr vim/* ~/.vim/
+
+git clone git@github.com:VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+vim +PluginInstall +qall
+
+cd ~/.vim/bundle/YouCompleteMe && ./install.py
